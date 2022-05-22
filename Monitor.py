7d1c07dcfdfd5d6e2eb7f1932ad0938fd9c6602b
Sebastian <smolanof@javeriana.edu.co>
2022-05-21 23:32:50 -0500

    Se agrega el monitor con la capacidad de recibir y almacenar las mediciones

--- Monitor.py
+++ Monitor.py
@@ -1,12 +1,54 @@
+from distutils import archive_util
 from sys import argv
+import zmq
+import Routing as ro
+import settings
+import datetime as dt
+from os.path import exists
+import json
+
+import settings
 
 
 class Monitor:
     tipo_monitor: int
+    mediciones: list
+    monitor_path: str
 
     def __init__(self, tipo_monitor) -> None:
-        pass
+        self.tipo_monitor = tipo_monitor
+        self.mediciones = []
+        self.monitor_path = f"monitor_db/monitor{self.tipo_monitor}.json"
+        self.correr()
 
+    def correr(self):
+        if exists(self.monitor_path):
+            f = open(self.monitor_path)
+            archivo = f.read()
+            if f != "":
+                self.mediciones = json.loads(archivo)
+            f.close()
+
+        context = zmq.Context()
+        socket: zmq.Socket = context.socket(zmq.SUB)
+        socket.connect(f"tcp://{ro.PROXYDIR}:{ro.PROXYOUTPORT}")
+        socket.setsockopt_string(zmq.SUBSCRIBE, f"{settings.tipos_sensor.get(self.tipo_monitor)}")
+
+        while True:
+            mensaje = socket.recv_string()
+            mensaje = mensaje.replace(f"{settings.tipos_sensor.get(self.tipo_monitor)}=", "")
+            self.agregarMedicion(str(dt.datetime.now()), mensaje)
+            print(mensaje)
+    
+    def agregarMedicion(self, fecha: str, medicion: float):
+        self.mediciones.append({"fecha" : fecha, "medicion" : medicion})
+        f = open(self.monitor_path, "w")
+        jsonw = json.dumps(self.mediciones)
+        f.write(jsonw)
+        f.close()
+
+    def verificarValor(self):
+        rangos_aceptables = settings.rangos_parametros_calidad.get(self.tipo_monitor)
 
 def main():
     if len(argv) != 2:
